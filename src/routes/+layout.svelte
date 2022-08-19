c165c2ebb41d0d31e4f034b25a14c13d8cac60fa
matievisthekat <matievisthekat@gmail.com>
2022-08-19 10:53:26 +0200

    add global layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import githubImg from '$lib/assets/github.png';
+</script>
+
+<nav>
+	<a href='/about'>About</a>
+	<a href='/top'>Top Users</a>
+	<a href='/login?next=/review'>Leave a Rating</a>
+	<a href='https://github.com/matievisthekat/rate-opensource-contributors' rel='external'>
+		<img src={githubImg} alt='GitHub' />
+	</a>
+</nav>
+
+<slot></slot>
